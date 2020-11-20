274f7b7d0520b2b3b50b9bf88a586cd9dcb5547c
positiveko <edie_ko@naver.com>
2020-11-20 13:13:28 +0900

    [Fix] Video Clip 컴포넌트 유튜브 API 활용 리스트업 기능 구현 - UI 디테일 수정 작업 중

--- src/pages/MovieDetail/MovieSide/MovieClip/MovieClip.js
+++ src/pages/MovieDetail/MovieSide/MovieClip/MovieClip.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 import './movieClip.scss';
 
 const API = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=bts&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`;
@@ -13,13 +14,8 @@ class MovieClip extends Component {
   }
 
   componentDidMount() {
-    this.getVideos();
+    this.callApi();
   }
-
-  getVideos = async () => {
-    const videos = await this.callApi();
-    this.setState({ videos });
-  };
 
   callApi = () => {
     return fetch(API, {
@@ -27,13 +23,41 @@ class MovieClip extends Component {
       redirect: 'follow',
     })
       .then((response) => response.json())
-      .then((result) => console.log(result))
+      .then((result) => this.setState({ videos: result }))
       .catch((error) => console.log('error', error));
   };
 
   render() {
-    console.log(this.state.videos);
-    return <div className='MovieClip'>videos</div>;
+    const { videos } = this.state;
+    console.log(videos.items);
+    const videoArr = videos.items;
+    console.log(videoArr);
+    return (
+      <div className='MovieClip'>
+        <ul>
+          {videoArr &&
+            videoArr.map((video) => (
+              <a
+                href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+                target='_blank'
+                key={video.id.videoId}>
+                <li>
+                  <div className='thumbnail'>
+                    <div className='playIcon'>
+                      <FontAwesomeIcon icon={faPlayCircle} />
+                    </div>
+                    <img
+                      src={video.snippet.thumbnails.default.url}
+                      alt='thumbnail'
+                    />
+                  </div>
+                  <div className='videoTitle'>{video.snippet.title}</div>
+                </li>
+              </a>
+            ))}
+        </ul>
+      </div>
+    );
   }
 }
 
